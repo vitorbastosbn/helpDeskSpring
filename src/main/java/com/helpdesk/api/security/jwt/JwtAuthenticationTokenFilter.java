16383b9e898972d1d3aed71785160abec2c2ab3e
Vitor Bastos <vitorbastosbn@gmail.com>
2018-04-15 23:10:16 -0300

    jh

--- src/main/java/com/helpdesk/api/security/jwt/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/helpdesk/api/security/jwt/JwtAuthenticationTokenFilter.java
@@ -44,7 +44,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
 		}
 		resposta.addHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-		resposta.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		resposta.addHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Authorization");
 		chain.doFilter(requisicao, resposta);
 
 	}
